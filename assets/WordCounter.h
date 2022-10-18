8a0f76befa0da54ac5e303d095ff428b344cc74c
MaxiLR <maximolr.itr@gmail.com>
2022-10-18 09:39:51 -0300

    added WordCounter.h

---
+++ assets/WordCounter.h
@@ -0,0 +1,64 @@
+#ifndef WORDCOUNTER_H
+#define WORDCOUNTER_H
+
+#include "DictionaryTree.h"
+#include "HashMapWCE.h"
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+class WordCounter {
+private:
+  unsigned int LetterCount, WordCount, LineCount, DiffWordCount;
+  string toUpper(string Str);
+
+public:
+  WordCounter();
+  unsigned int getLetterCount();
+  unsigned int getWordCount();
+  unsigned int getLineCount();
+  unsigned int getDiffWordCount();
+  void defaultUse(string Filename);
+};
+
+WordCounter::WordCounter() {
+  this->LetterCount = 0;
+  this->WordCount = 0;
+  this->LineCount = 0;
+  this->DiffWordCount = 0;
+}
+
+unsigned int WordCounter::getLetterCount() { return LetterCount; }
+
+unsigned int WordCounter::getWordCount() { return WordCount; }
+
+unsigned int WordCounter::getLineCount() { return LineCount; }
+
+unsigned int WordCounter::getDiffWordCount() { return DiffWordCount; }
+
+void WordCounter::defaultUse(string Filename) {
+  ifstream file(Filename);
+  HashMap HM(125000);
+  string word;
+  while (!file.eof()) {
+    file >> word;
+    LetterCount += word.length();
+    WordCount++;
+    HM.put(word);
+    if (file.peek() == '\n' || file.eof())
+      LineCount++;
+    DiffWordCount = HM.getHECounter();
+  }
+  file.close();
+}
+
+string WordCounter::toUpper(string Str) {
+  for (int i = 0; i < Str.length(); i++) {
+    Str[i] = toupper(Str[i]);
+  }
+  return Str;
+}
+
+#endif // WORDCOUNTER_H
--- main.cpp
+++ main.cpp
@@ -1,89 +1,98 @@
-#include "assets/DictionaryTree.h"
-#include "assets/HashMapWCE.h"
+#include "assets/WordCounter.h"
 #include <ctime>
 #include <fstream>
 #include <iostream>
 #include <math.h>
 
+#define TEXT_1 "txt.txt"
+#define TEXT_2 "Words.txt"
+#define TEXT_3 "mucho_texto.txt"
+
 using namespace std;
 
-string toUpper(string Str) {
-  for (int i = 0; i < Str.length(); i++) {
-    Str[i] = toupper(Str[i]);
-  }
-  return Str;
-}
-
 int main() {
-
-  string word;
-
-  // Hash:
 
   clock_t begin;
   begin = clock();
 
-  HashMap HMTest(500000);
+  WordCounter WC;
 
-  try {
-    ifstream file("mucho_texto.txt");
-    while (!file.eof()) {
-      file >> word;
-      HMTest.put(word);
-    }
-    file.close();
-    cout << HMTest.getCounter("ABRIR") << endl
-         << "Palabras Diferentes: " << HMTest.getHECounter() << endl;
-  } catch (int err) {
-    cerr << "Error " << err << "!" << endl;
-  }
+  WC.defaultUse(TEXT_1);
+
+  cout << "WORDS: " << WC.getWordCount() << " | LETTERS: " << WC.getLetterCount() << " | LINES: " << WC.getLineCount() << " | DIFFWORDS: " << WC.getDiffWordCount();
 
   clock_t end = clock();
   double elapsed_secs = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
-  cout << "Tiempo del Hash: " << elapsed_secs << "\n" << endl;
+  cout << endl << "Tiempo: " << elapsed_secs << "\n" << endl;
 
-  // Arbol:
+  // string word;
 
-  clock_t begin2;
-  begin2 = clock();
+  // // Hash:
 
-  BinaryTree BT;
+  // clock_t begin;
+  // begin = clock();
 
-  ifstream file2("mucho_texto.txt");
-  while (!file2.eof()) {
-    try {
-      file2 >> word;
-      BT.put(toUpper(word));
-    } catch (int err) {
-    }
-  }
-  file2.close();
+  // HashMap HMTest(500000);
 
-  cout << "Palabras diferentes: " << BT.getDiffCounter() << endl;
+  // try {
+  //   ifstream file("mucho_texto.txt");
+  //   while (!file.eof()) {
+  //     file >> word;
+  //     HMTest.put(word);
+  //   }
+  //   file.close();
+  //   cout << HMTest.getCounter("ABRIR") << endl
+  //        << "Palabras Diferentes: " << HMTest.getHECounter() << endl;
+  // } catch (int err) {
+  //   cerr << "Error " << err << "!" << endl;
+  // }
 
-  clock_t end2 = clock();
-  double elapsed_secs2 = static_cast<double>(end2 - begin2) / CLOCKS_PER_SEC;
-  cout << "Tiempo del Arbol: " << elapsed_secs2 << "\n" << endl;
+  // clock_t end = clock();
+  // double elapsed_secs = static_cast<double>(end - begin) / CLOCKS_PER_SEC;
+  // cout << "Tiempo del Hash: " << elapsed_secs << "\n" << endl;
 
-  // Arbol Dictionario:
+  // // Arbol:
 
-  clock_t begin3;
-  begin3 = clock();
+  // clock_t begin2;
+  // begin2 = clock();
 
-  DictionaryTree DT;
+  // BinaryTree BT;
 
-  ifstream file3("mucho_texto.txt");
-  while (!file3.eof()) {
-    file3 >> word;
-    DT.put(word);
-  }
-  file3.close();
+  // ifstream file2("mucho_texto.txt");
+  // while (!file2.eof()) {
+  //   try {
+  //     file2 >> word;
+  //     BT.put(toUpper(word));
+  //   } catch (int err) {
+  //   }
+  // }
+  // file2.close();
 
-  cout << "Palabras diferentes: " << DT.getDiffCounter() << endl;
+  // cout << "Palabras diferentes: " << BT.getDiffCounter() << endl;
 
-  clock_t end3 = clock();
-  double elapsed_secs3 = static_cast<double>(end3 - begin3) / CLOCKS_PER_SEC;
-  cout << "Tiempo del Arbol: " << elapsed_secs3 << "\n" << endl;
+  // clock_t end2 = clock();
+  // double elapsed_secs2 = static_cast<double>(end2 - begin2) / CLOCKS_PER_SEC;
+  // cout << "Tiempo del Arbol: " << elapsed_secs2 << "\n" << endl;
+
+  // // Arbol Dictionario:
+
+  // clock_t begin3;
+  // begin3 = clock();
+
+  // DictionaryTree DT;
+
+  // ifstream file3("mucho_texto.txt");
+  // while (!file3.eof()) {
+  //   file3 >> word;
+  //   DT.put(word);
+  // }
+  // file3.close();
+
+  // cout << "Palabras diferentes: " << DT.getDiffCounter() << endl;
+
+  // clock_t end3 = clock();
+  // double elapsed_secs3 = static_cast<double>(end3 - begin3) / CLOCKS_PER_SEC;
+  // cout << "Tiempo del Arbol: " << elapsed_secs3 << "\n" << endl;
 
   return 0;
 }
